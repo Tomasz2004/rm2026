b7d13c7805e2fb00e3c43caa1bdb72c42f9df51e
t-rogalski <tomaszrogalski04@gmail.com>
2025-12-02 17:48:13 +0100

    deploy

--- rmbmApp/vite.config.js
+++ rmbmApp/vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // eslint-disable-next-line no-undef
-  base: process.env.NODE_ENV === 'production' ? '/rm2026/' : '/',
+  base: '/',
 });
